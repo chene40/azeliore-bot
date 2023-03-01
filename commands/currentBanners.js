d15764f63d4b920e121f1c04ab179fce108d47d3
Eric <chene9802@gmail.com>
2023-02-28 19:11:07 -0500

    Added banner images to !currentbanners command

--- commands/currentBanners.js
+++ commands/currentBanners.js
@@ -2,11 +2,16 @@
 const CurrentBanners = require("../GenshinData/CurrentBanners.json");
 
 module.exports.run = (client, message, args) => {
-  let bannersAvailable = "The current available banners are:\n";
-  Object.keys(CurrentBanners).forEach(
-    (banner, i) => (bannersAvailable += `${i + 1}: ${banner} \n`)
-  );
-  message.reply(bannersAvailable);
+  message.channel.send("The current available banners are:\n");
+
+  Object.keys(CurrentBanners).forEach((banner, i) => {
+    const image = CurrentBanners[banner].image;
+    let msg = `${i + 1}: ${banner}\n`;
+
+    setTimeout(() => {
+      message.channel.send({ content: msg, files: [image] });
+    }, (i + 1) * 1000);
+  });
 };
 
 exports.name = "currentBanners".toLowerCase();
